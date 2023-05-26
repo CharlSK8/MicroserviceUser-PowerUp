fb9d5031fbdd28056be515106e5083092e227204
CharlSK8 <charlinson2013@gmail.com>
2023-05-26 16:26:07 -0500

    Responsedto is modified

--- src/main/java/com/pragma/powerup/usermicroservice/adapters/driving/http/mapper/IOwnerRequestMapper.java
+++ src/main/java/com/pragma/powerup/usermicroservice/adapters/driving/http/mapper/IOwnerRequestMapper.java
@@ -1,7 +1,6 @@
 package com.pragma.powerup.usermicroservice.adapters.driving.http.mapper;
 
 import com.pragma.powerup.usermicroservice.adapters.driving.http.dto.request.OwnerRequestDto;
-import com.pragma.powerup.usermicroservice.adapters.driving.http.dto.response.OwnerResponseDto;
 import com.pragma.powerup.usermicroservice.domain.model.Owner;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
